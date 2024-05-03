0e8277c90dd85eb75d297e9186bdc51163c97ecb
Jiahan Chen <3238468729@qq.com>
2024-05-03 19:06:57 +0100

    Phase 1 add rejection mailer when the image extension is not .jpeg or .png

--- lambdas/processImage.ts
+++ lambdas/processImage.ts
@@ -1,41 +1,49 @@
 /* eslint-disable import/extensions, import/no-absolute-path */
-import { SQSHandler } from "aws-lambda";
+import {Context, SQSEvent, SQSHandler} from "aws-lambda";
 import {
-  GetObjectCommand,
-  PutObjectCommandInput,
-  GetObjectCommandInput,
-  S3Client,
-  PutObjectCommand,
+    GetObjectCommand,
+    PutObjectCommandInput,
+    GetObjectCommandInput,
+    S3Client,
+    PutObjectCommand,
 } from "@aws-sdk/client-s3";
+import * as path from "path";
 
 const s3 = new S3Client();
 
-export const handler: SQSHandler = async (event) => {
-  console.log("Event ", JSON.stringify(event));
-  for (const record of event.Records) {
-    const recordBody = JSON.parse(record.body);  // Parse SQS message
-    const snsMessage = JSON.parse(recordBody.Message); // Parse SNS message
+export const handler: SQSHandler = async (event: SQSEvent, context: Context) => {
+    console.log("Event ", JSON.stringify(event));
+    for (const record of event.Records) {
+        const recordBody = JSON.parse(record.body);  // Parse SQS message
+        const snsMessage = JSON.parse(recordBody.Message); // Parse SNS message
 
-    if (snsMessage.Records) {
-      console.log("Record body ", JSON.stringify(snsMessage));
-      for (const messageRecord of snsMessage.Records) {
-        const s3e = messageRecord.s3;
-        const srcBucket = s3e.bucket.name;
-        // Object key may have spaces or unicode non-ASCII characters.
-        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
-        let origimage = null;
-        try {
-          // Download the image from the S3 source bucket.
-          const params: GetObjectCommandInput = {
-            Bucket: srcBucket,
-            Key: srcKey,
-          };
-          origimage = await s3.send(new GetObjectCommand(params));
-          // Process the image ......
-        } catch (error) {
-          console.log(error);
+        if (snsMessage.Records) {
+            console.log("Record body ", JSON.stringify(snsMessage));
+            for (const messageRecord of snsMessage.Records) {
+                const s3e = messageRecord.s3;
+                const srcBucket = s3e.bucket.name;
+                // Object key may have spaces or unicode non-ASCII characters.
+                const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
+                let origimage = null;
+                try {
+                    // Download the image from the S3 source bucket.
+                    const params: GetObjectCommandInput = {
+                        Bucket: srcBucket,
+                        Key: srcKey,
+                    };
+
+                    // jpg and png only
+                    const extension = path.extname(srcKey);
+                    if (extension !== '.jpeg' && extension !== '.png') {
+                        throw new Error(`Invalid file type: ${extension}`);
+                    }
+
+                    origimage = await s3.send(new GetObjectCommand(params));
+                    // Process the image ......
+                } catch (error) {
+                    console.log(error);
+                }
+            }
         }
-      }
     }
-  }
 };
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -9,95 +9,132 @@ import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as iam from "aws-cdk-lib/aws-iam";
 
-import { Construct } from "constructs";
+import {Construct} from "constructs";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const imagesBucket = new s3.Bucket(this, "images", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      publicReadAccess: false,
-    });
+        const imagesBucket = new s3.Bucket(this, "images", {
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+            publicReadAccess: false,
+        });
 
-  // Integration infrastructure
+        // Integration infrastructure
 
-  const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
-    receiveMessageWaitTime: cdk.Duration.seconds(10),
-  });
+        // rejected mails queue
+        const rejectedMailsQueue = new sqs.Queue(this, "rejected-mailer-queue", {
+            receiveMessageWaitTime: cdk.Duration.seconds(10),
+        });
 
-  const newImageTopic = new sns.Topic(this, "NewImageTopic", {
-    displayName: "New Image topic",
-  }); 
+        const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
+            receiveMessageWaitTime: cdk.Duration.seconds(10),
+            deadLetterQueue: {
+                queue: rejectedMailsQueue,
+                // # of rejections by consumer (lambda function)
+                maxReceiveCount: 1,
+            },
+        });
 
-  const mailerQ = new sqs.Queue(this, "mailer-queue", {
-    receiveMessageWaitTime: cdk.Duration.seconds(10),
-  });
+        const newImageTopic = new sns.Topic(this, "NewImageTopic", {
+            displayName: "New Image topic",
+        });
 
-  // Lambda functions
+        const mailerQ = new sqs.Queue(this, "mailer-queue", {
+            receiveMessageWaitTime: cdk.Duration.seconds(10),
+        });
 
-  const processImageFn = new lambdanode.NodejsFunction(
-    this,
-    "ProcessImageFn",
-    {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      entry: `${__dirname}/../lambdas/processImage.ts`,
-      timeout: cdk.Duration.seconds(15),
-      memorySize: 128,
+        // Lambda functions
+
+        const processImageFn = new lambdanode.NodejsFunction(
+            this,
+            "ProcessImageFn",
+            {
+                runtime: lambda.Runtime.NODEJS_18_X,
+                entry: `${__dirname}/../lambdas/processImage.ts`,
+                timeout: cdk.Duration.seconds(15),
+                memorySize: 128,
+            }
+        );
+
+        const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            memorySize: 1024,
+            timeout: cdk.Duration.seconds(3),
+            entry: `${__dirname}/../lambdas/mailer.ts`,
+        });
+
+        const rejectionMailerFn = new lambdanode.NodejsFunction(this, "rejection-mailer-function", {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            memorySize: 1024,
+            timeout: cdk.Duration.seconds(3),
+            entry: `${__dirname}/../lambdas/rejectionMailer.ts`,
+        });
+
+        // S3 --> SQS
+        imagesBucket.addEventNotification(
+            s3.EventType.OBJECT_CREATED,
+            new s3n.SnsDestination(newImageTopic)  // Changed
+        );
+
+        // SQS --> Lambda
+        const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
+            batchSize: 5,
+            maxBatchingWindow: cdk.Duration.seconds(10),
+        });
+        const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+            batchSize: 5,
+            maxBatchingWindow: cdk.Duration.seconds(10),
+        });
+        const newImageRejectionMailEventSource = new events.SqsEventSource(rejectedMailsQueue, {
+            maxBatchingWindow: cdk.Duration.seconds(5),
+            maxConcurrency: 2,
+        });
+
+        newImageTopic.addSubscription(
+            new subs.SqsSubscription(imageProcessQueue)
+        );
+        newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+        newImageTopic.addSubscription(new subs.SqsSubscription(rejectedMailsQueue));
+
+        processImageFn.addEventSource(newImageEventSource);
+        mailerFn.addEventSource(newImageMailEventSource);
+        rejectionMailerFn.addEventSource(newImageRejectionMailEventSource);
+
+        // Permissions
+
+        imagesBucket.grantRead(processImageFn);
+        mailerFn.addToRolePolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "ses:SendEmail",
+                    "ses:SendRawEmail",
+                    "ses:SendTemplatedEmail",
+                ],
+                resources: ["*"],
+            })
+        );
+        // Add SES permissions to the rejection mailer function
+        rejectionMailerFn.addToRolePolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "ses:SendEmail",
+                    "ses:SendRawEmail",
+                    "ses:SendTemplatedEmail",
+                ],
+                resources: ["*"],
+            })
+        );
+
+        // Output
+
+        new cdk.CfnOutput(this, "bucketName", {
+            value: imagesBucket.bucketName,
+        });
     }
-  );
-
-  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
-    runtime: lambda.Runtime.NODEJS_16_X,
-    memorySize: 1024,
-    timeout: cdk.Duration.seconds(3),
-    entry: `${__dirname}/../lambdas/mailer.ts`,
-  });
-
- // S3 --> SQS
-    imagesBucket.addEventNotification(
-        s3.EventType.OBJECT_CREATED,
-        new s3n.SnsDestination(newImageTopic)  // Changed
-    );
-
- // SQS --> Lambda
-  const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
-    batchSize: 5,
-    maxBatchingWindow: cdk.Duration.seconds(10),
-  });
-
-  newImageTopic.addSubscription(
-    new subs.SqsSubscription(imageProcessQueue)
-  );
-  newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
-  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
-    batchSize: 5,
-    maxBatchingWindow: cdk.Duration.seconds(10),
-  }); 
-  processImageFn.addEventSource(newImageEventSource);
-  mailerFn.addEventSource(newImageMailEventSource);
-
-  // Permissions
-
-  imagesBucket.grantRead(processImageFn);
-  mailerFn.addToRolePolicy(
-    new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: [
-        "ses:SendEmail",
-        "ses:SendRawEmail",
-        "ses:SendTemplatedEmail",
-      ],
-      resources: ["*"],
-    })
-  );
-  
-    // Output
-    
-    new cdk.CfnOutput(this, "bucketName", {
-      value: imagesBucket.bucketName,
-    });
-  }
 }
